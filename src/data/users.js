896ab841cf22b3760d19b9887206880eb2c69746
Marcin <mchdev@DESKTOP-ALQLME9.localdomain>
2021-02-02 16:43:23 +0100

    first components added

---
+++ src/data/users.js
@@ -0,0 +1,31 @@
+export const users = [
+  {
+    name: 'Marcin Chmaj',
+    attendance: '99%',
+    average: '5.3',
+  },
+  {
+    name: 'Tyler Kowalski',
+    attendance: '59%',
+    average: '3.3',
+  },
+  {
+    name: 'Jan Nowak',
+    attendance: '29%',
+    average: '2.3',
+  },
+  {
+    name: 'Stefan Kowalski',
+    attendance: '79%',
+    average: '4.3',
+  },
+  {
+    name: 'Alfred Nowak',
+    attendance: '89%',
+    average: '2.7',
+  },
+  {
+    name: 'Michał Nowak',
+    average: '1.5',
+  },
+];
